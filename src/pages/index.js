60e1470d2e9ecf911db2282c517369f93eec81e0
Piotr Pupczyk <p.pupczyk@samsung.com>
2019-02-25 15:55:28 +0100

    add assets

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-
 import ImageContainer from "../components/ImageContainer";
 import About from "../components/About";
 import LastProjects from "../components/LastProjects";
 import OrientationLock from "../components/OrientationLock";
 import SEO from "../components/seo"
 import "../styles/main.scss";
+import "normalize.css/normalize.css";
 
 const IndexPage = (query) => (
   <>
