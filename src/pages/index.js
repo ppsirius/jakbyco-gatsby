e227ce34cf19c8c0a58582030ce9f89169b94211
Piotr Pupczyk <p.pupczyk@samsung.com>
2019-03-14 17:00:38 +0100

    update title

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import "normalize.css/normalize.css";
 
 const IndexPage = (query) => (
   <>
-    <SEO title="Hi" keywords={[`portfolio`, `frontend`, `developer`, `react`, `gatsby`, `animation`]} />
+    <SEO keywords={[`portfolio`, `frontend`, `developer`, `react`, `gatsby`, `animation`]} />
     <div className="app">
       <ImageContainer />
       <About />
