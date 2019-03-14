9e668ea889168338fa141df5afed38217509c2fb
Piotr Pupczyk <p.pupczyk@samsung.com>
2019-03-14 18:22:19 +0100

    update 404 text

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,7 +12,7 @@ const styles = {
 const NotFoundPage = () => (
   <div style={styles}>
     <SEO title="404: Not found" />
-    <h1>404: NOT FOUND</h1>
+    <h1>404: Page Not Found</h1>
     <p><iframe src="https://giphy.com/embed/umMYB9u0rpJyE" width="480" height="452" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/shocked-ernie-bert-umMYB9u0rpJyE"></a></p></p>
   </div>
 )
