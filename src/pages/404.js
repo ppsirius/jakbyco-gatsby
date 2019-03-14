773367d1b112af7b1d38aafaef30144176dc49f0
Piotr Pupczyk <p.pupczyk@samsung.com>
2019-03-14 18:15:11 +0100

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,18 @@ import React from "react"
 
 import SEO from "../components/seo"
 
+const styles = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column'
+}
+
 const NotFoundPage = () => (
-  <div>
+  <div style={styles}>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404: NOT FOUND</h1>
+    <p><iframe src="https://giphy.com/embed/umMYB9u0rpJyE" width="480" height="452" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/shocked-ernie-bert-umMYB9u0rpJyE"></a></p></p>
   </div>
 )
 
