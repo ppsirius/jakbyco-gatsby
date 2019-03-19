afd83d9a4a8ceaaf23e718552a59f45e0024b827
Piotr Pupczyk <p.pupczyk@samsung.com>
2019-03-19 16:02:46 +0100

    404 ifram width

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,18 +2,22 @@ import React from "react"
 
 import SEO from "../components/seo"
 
-const styles = {
+const containerStyle = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
   flexDirection: 'column'
 }
 
+const embedStyle = {
+  width: '100%'
+}
+
 const NotFoundPage = () => (
-  <div style={styles}>
+  <div style={containerStyle}>
     <SEO title="404: Not found" />
     <h1>404: Page Not Found</h1>
-    <p><iframe src="https://giphy.com/embed/umMYB9u0rpJyE" width="480" height="452" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/shocked-ernie-bert-umMYB9u0rpJyE"></a></p></p>
+    <p><iframe src="https://giphy.com/embed/umMYB9u0rpJyE" style={embedStyle} width="480" height="452" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/shocked-ernie-bert-umMYB9u0rpJyE"></a></p></p>
   </div>
 )
 
