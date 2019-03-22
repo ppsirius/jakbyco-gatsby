d665b37c13e473cc01265f82b5d247c039c0a507
Piotr Pupczyk <p.pupczyk@samsung.com>
2019-03-22 14:39:05 +0100

    add sentry (error tracking)

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,5 +61,14 @@ module.exports = {
         trackingId: "UA-27997837-1",
       },
     },
+    {
+      resolve: "gatsby-plugin-sentry",
+      options: {
+        dsn: "https://c09837bfcb6a42728aa8b81f84f9b2c8@sentry.io/1421652",
+        // Optional settings, see https://docs.sentry.io/clients/node/config/#optional-settings
+        environment: process.env.NODE_ENV,
+        enabled: (() => ["production"].indexOf(process.env.NODE_ENV) !== -1)()
+      }
+    }
   ],
 }
--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import Logo from "./Logo";
 import Image from "./Image";
 import SocialContainer from "./SocialContainer";
 
-const ImageContainer = () => {
-  return (
-    <div className="image-container">
-      <Image />
-      <Logo />
-      <SocialContainer />
-    </div>
-  );
+class ImageContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    logErrorToMyService(error, info);
+  }
+
+  render() {
+    return (
+      <div className="image-container">
+        <Image />
+        <Logo />
+        <SocialContainer />
+      </div>
+    );
+  }
 };
 
 export default ImageContainer;
