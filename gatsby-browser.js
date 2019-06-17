6844248795d914e665364d37eb7ebe7369b6ef38
Piotr Pupczyk <ppsirius@gmail.com>
2019-06-17 22:54:37 +0200

    problem with cashed files in sw

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,23 @@
-export { default as wrapRootElement } from './src/state/reduxWrapper';
+export { default as wrapRootElement } from "./src/state/reduxWrapper";
 
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
-  )
+// export const onServiceWorkerUpdateReady = () => {
+//   const answer = window.confirm(
+//     `This application has been updated. ` +
+//       `Reload to display the latest version?`
+//   )
 
-  if (answer === true) {
-    window.location.reload()
-  }
-}
+//   if (answer === true) {
+//     window.location.reload()
+//   }
+// }
+
+self.addEventListener("activate", function(e) {
+  self.registration
+    .unregister()
+    .then(function() {
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url));
+    });
+});
