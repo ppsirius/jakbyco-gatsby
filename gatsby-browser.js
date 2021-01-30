18ef4111ed8b45fe0987d9ee86e1f06dba556da6
Piotr Pupczyk <ppsirius@gmail.com>
2021-01-30 23:42:56 +0100

    update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,27 +10,27 @@ export { default as wrapRootElement } from "./src/state/reduxWrapper";
 //     window.location.reload()
 //   }
 // }
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames
-          .filter(function(cacheName) {
-            // Return true if you want to remove this cache,
-            // but remember that caches are shared across
-            // the whole origin
-          })
-          .map(function(cacheName) {
-            return caches.delete(cacheName);
-          })
-      );
-    })
-  );
-});
+// self.addEventListener("activate", function(event) {
+//   event.waitUntil(
+//     caches.keys().then(function(cacheNames) {
+//       return Promise.all(
+//         cacheNames
+//           .filter(function(cacheName) {
+//             // Return true if you want to remove this cache,
+//             // but remember that caches are shared across
+//             // the whole origin
+//           })
+//           .map(function(cacheName) {
+//             return caches.delete(cacheName);
+//           })
+//       );
+//     })
+//   );
+// });
 
 // Temporary solution unregister
-navigator.serviceWorker.getRegistrations().then(registrations => {
-  registrations.forEach(registration => {
+navigator.serviceWorker.getRegistrations().then((registrations) => {
+  registrations.forEach((registration) => {
     registration.unregister();
   });
 });
